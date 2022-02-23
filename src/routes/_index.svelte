29ec83f389da250e58e81a4490aa285d432d188f
lorenzoh <lorenz.ohly@gmail.com>
2022-02-23 19:21:59 +0100

    Remove failing redirect

--- src/routes/_index.svelte
+++
@@ -1,9 +0,0 @@
-<script context="module">
-	import { base } from '$app/paths';
-
-	export const prerender = true;
-	export const hydrate = true;
-</script>
-
-<a href="{base}/docs/dev/documents/README.md">Click me!</a>
-<a href="{base}/docs/dev/i">Click me!</a>
